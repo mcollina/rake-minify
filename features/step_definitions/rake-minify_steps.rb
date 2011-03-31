fff20951ad1609a29226f400c724469abc942c61
Matteo Collina <matteo.collina@gmail.com>
2011-03-31 11:56:08 +0200

    Extracted steps utils to their own file.

--- features/step_definitions/rake-minify_steps.rb
+++ features/step_definitions/rake-minify_steps.rb
@@ -1,40 +1,6 @@
-require 'erb'
 require 'fileutils'
 require 'tmpdir'
 require 'rake'
-
-class Group
-  attr_reader :sources
-  attr_reader :output
-  attr_reader :minify
-
-  def initialize(output, minify)
-    @output = output
-    @sources = []
-    @minify = minify
-  end
-end
-
-def groups
-  @groups ||= {}
-end
-
-def add_to_groups(output, minify, file)
-  groups[output] ||= Group.new(output, minify)
-  groups[output].sources << file
-end
-
-def generate_rakefile
-  template = <<-EOF
-Rake::Minify.new do |conf|
-  <% groups.values.each do |group| %>
-  conf.add(<%= group.output %>, <%= group.minify %>, <%= group.sources.inspect %>)
-  <% end %>
-end
-EOF
-  erb = ERB.new(template)
-  erb.result(binding)
-end
 
 Given /^we want to minify the js file "([^"]*)" into "([^"]*)"$/ do |file, output|
   add_to_groups(output, true, file)
